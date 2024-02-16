6ddb359536a1162dcf20f4b0d14acfafb8256a3c
Denton Underwood <denton.underwood@gmail.com>
2024-02-15 22:17:34 -0600

    Incomplete Passport implementation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 
@@ -11,6 +13,21 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 const port = process.env.PORT || 3001;
+
+app.use(session( {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.post('/login', passport.authenticate('local',  {
+    successRedirect: console.log('Success'),
+    failureRedirect: console.log('Failure')
+}))
+
 app.listen(port, () => {
     console.log('Started server on port', port);
 })
