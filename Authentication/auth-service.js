7af079a81df5cfa0fb6f9bbea6bf127549e260cc
Denton Underwood <denton.underwood@gmail.com>
2024-02-20 21:33:48 -0600

    Deleted duplicate code.

--- Authentication/auth-service.js
+++ Authentication/auth-service.js
@@ -1,20 +1,9 @@
-/* const repo = require('./auth-repo.js');
-
-const authenticateUser = async (username, password) => {
-    return await repo.authenticateUser(username, password);
-}
-
-module.exports = {
-    authenticateUser,
-} */
-
-// ------------------------------------------------------------
-// Authentication/auth-service
-// This is what you currently have so no changes needed
 const repo = require('./auth-repo.js');
+
 const authenticateUser = async (username, password) => {
     return await repo.authenticateUser(username, password);
 }
+
 module.exports = {
     authenticateUser,
 }
