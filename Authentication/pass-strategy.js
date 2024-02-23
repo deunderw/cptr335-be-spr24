54fb8d6710de97286d0ff355293ba0dc6629e82f
gadicker <gadicker@ucollege.edu>
2024-02-23 09:55:29 -0600

    Revert "Fixed pass-strategy function to return true and false correctly. However, deserialize function has a problem."

--- Authentication/pass-strategy.js
+++ Authentication/pass-strategy.js
@@ -1,8 +1,22 @@
+/* const localStrategy = require('passport-local').Strategy;
+
+passport.use(new localStrategy (authUser))
+
+authUser = (username, password, done) => {
+    let authenticatedUser = {username: 'Mickey', password: 'Password'}
+
+    return done (null, authenticatedUser)
+} */
+
+// ---------------------------------------------------------------------
+// Authentication/pass-startegy.js
+// should be something like this
+// note that for this to work there needs to be a Users/users-service
+// (and Users/users-repo created also)  Possible routines defined later
 const passport = require("passport");
 const Strategy = require("passport-local").Strategy;
 const authService = require("./auth-service");
 const userService = require("../Users/users-service");
-
 passport.use(
   "local",
   new Strategy(
@@ -13,10 +27,10 @@ passport.use(
     (username, password, cb) => {
       return authService.authenticateUser(username, password)
         .then(user => {
-          if (!username) {
+          if (!user) {
             return cb(null, false);
           }
-          return cb(null, username);
+          return cb(null, user);
         })
         .catch((err) => cb(err));
     },
@@ -29,14 +43,13 @@ passport.use(
 // typical implementation of this is as simple as supplying the user ID when
 // serializing, and querying the user record by ID from the database when
 // deserializing.
-passport.serializeUser((username, cb) => {
-    cb(null, { id: username.clientId });
+passport.serializeUser((user, cb) => {
+    cb(null, { id: user.clientId });
 });
 
 passport.deserializeUser((obj, cb) => {
       userService.getById(obj.id)
         .then((user) => {
-            console.log(user);
           cb(null, user);
         })
         .catch((err) => cb(err));
