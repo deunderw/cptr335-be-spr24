d5c2f483065946bdb7bda9ee143a7495637ea960
Denton Underwood <denton.underwood@gmail.com>
2024-02-23 01:40:57 -0600

    Added debug statements trying to figure out why auth-controller.js isn't getting correct data from pass-strategy.js

--- Authentication/auth-repo.js
+++ Authentication/auth-repo.js
@@ -1,33 +1,9 @@
-/* const nano = require('nano')('http://${process.env.DBUSER}:${process.env.DBPASS}@localhost:5984');
-const db = nano.db.use('stocks');
-
-const authenticateUser = async (username, password) => {
-    const doc = await db.find({
-        "selector":{
-            "username":{
-                "$eq": username
-            },
-            "password": {
-                "$eq": password
-            }
-        }
-    });
-
-    return doc.docs[0];
-}
-
-module.exports = {
-    authenticateUser,
-} */
-
-// --------------------------------------------------------------------------------------------
-// Authentication/auth-repo
-// I would suggest commenting out the db call for right now and return
-// something like below.  It doesn't have to be exactly this but it should
-// be something you both agree on
 const nano = require('nano')('http://${process.env.DBUSER}:${process.env.DBPASS}@localhost:5984');
 const db = nano.db.use('stocks');
+
+console.log('auth-repo file'); // debug
 const authenticateUser = async (username, password) => {
+    console.log('authenticateUser in auth-repo'); //debug
 //    const doc = await db.find({
 //        "selector":{
 //            "username":{
--- Authentication/auth-service.js
+++ Authentication/auth-service.js
@@ -1,5 +1,6 @@
 const repo = require('./auth-repo.js');
 
+console.log("auth-service file"); // debug
 const authenticateUser = async (username, password) => {
     return await repo.authenticateUser(username, password);
 }
