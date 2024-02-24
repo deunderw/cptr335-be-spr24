2be54e5410f807562e87789e1dd6282b5ce3833e
Denton Underwood <denton.underwood@gmail.com>
2024-02-24 00:07:31 -0600

    Fixed return object syntax.

--- Users/users-repo.js
+++ Users/users-repo.js
@@ -2,13 +2,12 @@
 // Once again this is for testing.  We would need to hook this up to the db
 // later but right now we will just stub it would with static data
 const getById = async (id) => {
-    return 
-        {
-            firstName = 'John',
-            lastName = 'Doe',
-            email = 'john@example.com',
-            clientId = '1'
-        };    
+    return {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john@example.com',
+        clientId: '1'
+    };    
 }
 module.exports = {
    getById,
