cc2f6d5ded380694e60f995a346a93531f2c69de
gary-hitsend <157754918+gary-hitsend@users.noreply.github.com>
2024-02-24 21:41:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const app = express();
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept",
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 })
 app.use(bodyParser.json({ limit: "50mb" }));
