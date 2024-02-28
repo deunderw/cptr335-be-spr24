05ae4d88a8d6b0a032d6ba397cd30921de8c62f7
Denton Underwood <denton.underwood@gmail.com>
2024-02-27 19:18:57 -0600

    Implemented bcrypt on account password check

--- Authentication/auth-repo.js
+++ Authentication/auth-repo.js
@@ -1,5 +1,6 @@
 const nano = require('nano')(`http://${process.env.DBUSER}:${process.env.DBPASS}@localhost:5984`);
 const db = nano.db.use(`${process.env.DBNAME}`);
+const bcrypt = require('bcrypt');
 
 const authenticateUser = async (username, password) => {
     try {
@@ -7,7 +8,6 @@ const authenticateUser = async (username, password) => {
         const result = await db.find({
             selector: {
                 "email": { "$eq": username },
-                "password": { "$eq": password }
             },
             limit: 1 // Limit to one result (assuming username and password are unique)
         });
@@ -16,14 +16,21 @@ const authenticateUser = async (username, password) => {
         if (result.docs.length > 0) {
             const user = result.docs[0];
 
-            // Return the user data
-            return {
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                clientId: user.clientId
-                // Include other fields as needed
-            };
+            // Compare the provided password with the hashed password stored in the database
+            const passwordMatch = await bcrypt.compare(password, user.password);
+            if (passwordMatch) {
+                // Return the user data
+                return {
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+                    clientId: user.clientId
+                    // Include other fields as needed
+                };
+            } else {
+                // Password doesn't match
+                return null;
+            }
         } else {
             // No user found
             return null;
