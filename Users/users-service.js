6ca8cd3d06bef87adb0680395693856cb2688a9c
Denton Underwood <denton.underwood@gmail.com>
2024-03-01 21:29:42 -0600

    Fixed deserialize function error and added null checking to email query before checking password hash.

--- Users/users-service.js
+++ Users/users-service.js
@@ -1,9 +1,8 @@
-const userRepository = require('./user-repository');
 const repo = require('./users-repo.js');
 
 async function createUser(username, password, email) {
 
-    return await userRepository.createUser(username, password, email);
+    return await repo.createUser(username, password, email);
 }
 
 const getById = async (id) => {
