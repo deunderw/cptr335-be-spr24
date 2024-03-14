fee140bbec280e6df2081335d2392fe537a3220e
Nathan <nathangsweet@gmail.com>
2024-03-14 12:01:31 -0400

    Make sure email isn't in use

--- Users/users-service.js
+++ Users/users-service.js
@@ -1,6 +1,11 @@
 const repo = require('./users-repo.js');
 
 async function createUser(firstName, lastName, email, password) {
+    const emailInUse = await repo.isEmailInUse(email);
+
+    if (emailInUse) {
+        throw new Error('Email is already in use');
+    }
 
     return await repo.createUser(firstName, lastName, email, password);
 }
