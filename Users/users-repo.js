284a4d7e15e1ab00b95a248e4ee17251dd71b044
Denton Underwood <denton.underwood@gmail.com>
2024-04-05 23:32:42 -0500

    minor refactor

--- Users/users-repo.js
+++ Users/users-repo.js
@@ -22,11 +22,11 @@ async function createUser(firstName, lastName, email, password) {
         throw new Error('Email is already in use');
     }
 
-    const userId = uuidv4();
+    const id = uuidv4();
     const hashedPassword = await bcrypt.hash(password, 10);
     
     const newUser = {
-        id: userId,
+        id,
         firstName,
         lastName,
         email,
@@ -37,7 +37,7 @@ async function createUser(firstName, lastName, email, password) {
     const response = await db.insert(newUser);
     console.log('response', response);
 
-    return { id: userId, ...newUser };
+    return { id, ...newUser };
 }
 
 async function updateUser(userID, firstName, lastName, email) {
--- Users/users-service.js
+++ Users/users-service.js
@@ -15,21 +15,16 @@ const getById = async (id) => {
 }
 
 async function updateUser(userID, firstName, lastName, email, formEmail) {
-    if(email != formEmail) {
-        const emailInUse = await repo.isEmailInUse(email);
-        if(emailInUse) {
-            throw new Error('Email is already in use!');
-        }else {
-            return await repo.updateUser(userID, firstName, lastName, email);
-        }
-    }else {
-        const updateResult = await repo.updateUser(userID, firstName, lastName, email);
-        if (updateResult) {
-            return { success: true };
-        } else {
-            throw new Error('Failed to update user!');
-        }
+    const emailInUse = await repo.isEmailInUse(email);
+    if (email != formEmail && emailInUse) {
+        throw new Error('Email is already in use!');
     }
+
+    const updateResult = await repo.updateUser(userID, firstName, lastName, email);
+    if (!updateResult) {
+        throw new Error('Failed to update user!');
+    }
+    return updateResult;
 };
 
 module.exports = { createUser, getById, updateUser };
