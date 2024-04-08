875aac79e9a21a06ab53069e774c282439bec613
Denton Underwood <denton.underwood@gmail.com>
2024-04-08 09:21:05 -0500

    Format changes using prettier package.  No logic changes!

--- Stocks/stocks-controlller.js
+++ Stocks/stocks-controlller.js
@@ -1,18 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = (module.exports = express());
-const service = require('./stocks-service'); 
+const service = require('./stocks-service');
 
 app.put('/be/stock/:symbol', async (req, res) => {
-    const results = await service.updateStockPrice(req.params.symbol);
-    if (results.err) {
-        res.sendStatus(404).send({error: results.err});
-    } else {
-        res.sendStatus(200);
-    }
-})
+  const results = await service.updateStockPrice(req.params.symbol);
+  if (results.err) {
+    res.sendStatus(404).send({ error: results.err });
+  } else {
+    res.sendStatus(200);
+  }
+});
 
 app.post('/be/initializeDB', async (req, res) => {
-    const data = req.body;
-    await service.initializeDB(data);
-    res.sendStatus(200);
-})
+  const data = req.body;
+  await service.initializeDB(data);
+  res.sendStatus(200);
+});
